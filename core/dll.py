49e70935fabe3587c47141430c3ec603923a87bf
JozfPro <balcikonis.juozas@gmail.com>
2024-11-30 13:09:12 +0200

    feature: made dll allow array input

--- core/dll.py
+++ core/dll.py
@@ -2,51 +2,39 @@ class Dll:
     def __init__(self, version):
         self.MAILBOX_SIZE = 4096
         self.state = 'NSYNC'
-        self.dataBuffer = []
-        self.dllBuffer = []
+        self.dataBuffer = bytearray()
+        self.dllBuffer = bytearray()
         self.escFlag = False
         self.type = version
 
-    def dllPack(self, inputBuffer):
-        self.dllBuffer = ['S']
-        for char in inputBuffer:
-            if char in ['S', '/', 'E']:
-                self.dllBuffer.append('/')
-            self.dllBuffer.append(char)
+    def dllPack(self, inputBuffer: bytearray) -> bytearray:
+        self.dllBuffer = bytearray(b'S')
+        for byte in inputBuffer:
+            if byte in [ord('S'), ord('/'), ord('E')]:
+                self.dllBuffer.append(ord('/'))
+            self.dllBuffer.append(byte)
         if self.type == 1:
-            self.dllBuffer.append('E')
+            self.dllBuffer.append(ord('E'))
+        return self.dllBuffer
 
-    def dllRcv(self, char):
-        if self.state == 'NSYNC':
-            if char == 'S':
-                self.state = 'SYNC'
-                self.dataBuffer = []
-                self.escFlag = False
-        elif self.state == 'SYNC':
-            if char == 'S' and not self.escFlag:
-                self.dataBuffer = []
-            elif char == 'E' and not self.escFlag:
-                self.state = 'NSYNC'
-            elif char == '/':
-                self.escFlag = not self.escFlag
-            else:
-                if self.escFlag:
+    def dllRcv(self, inputBuffer: bytearray) -> bytearray:
+        for byte in inputBuffer:
+            if self.state == 'NSYNC':
+                if byte == ord('S'):
+                    self.state = 'SYNC'
+                    self.dataBuffer = bytearray()
                     self.escFlag = False
-                self.dataBuffer.append(char)
-        else:
-            print(f"ERROR: DLL in illegal state ({self.state})")
-
-# Example usage
-dll = Dll(version=1)
-input_data = ['a', 'b', '/', 'S', 'e', 'f', 'g', 'h', 'i', 'j']
-
-# Serialize the input
-dll.dllPack(input_data)
-print("Serialized data:", ''.join(dll.dllBuffer))
-
-# Deserialize the data
-for c in dll.dllBuffer:
-    dll.dllRcv(c)
-
-if dll.dataBuffer:
-    print("Deserialized output:", ''.join(dll.dataBuffer))
+            elif self.state == 'SYNC':
+                if byte == ord('S') and not self.escFlag:
+                    self.dataBuffer = bytearray()
+                elif byte == ord('E') and not self.escFlag:
+                    self.state = 'NSYNC'
+                elif byte == ord('/'):
+                    self.escFlag = not self.escFlag
+                else:
+                    if self.escFlag:
+                        self.escFlag = False
+                    self.dataBuffer.append(byte)
+            else:
+                print(f"ERROR: DLL in illegal state ({self.state})")
+        return self.dataBuffer
