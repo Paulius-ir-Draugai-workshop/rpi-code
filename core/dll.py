dbc67fe3714d1aa01212883e58ce727f983c66d9
Bilgis <paulius.sabanskis@gmail.com>
2024-11-30 16:14:02 +0200

    dll code clenaup

--- core/dll.py
+++ core/dll.py
@@ -1,7 +1,6 @@
 class Dll:
     def __init__(self):
         self.state = "NSYNC"
-        self.dataBuffer = bytearray()
         self.dllBuffer = bytearray()
         self.escFlag = False
 
@@ -13,25 +12,3 @@ class Dll:
             self.dllBuffer.append(byte)
         self.dllBuffer.append(ord("E"))
         return self.dllBuffer
-
-    def dllRcv(self, inputBuffer: bytearray) -> bytearray:
-        for byte in inputBuffer:
-            if self.state == "NSYNC":
-                if byte == ord("S"):
-                    self.state = "SYNC"
-                    self.dataBuffer = bytearray()
-                    self.escFlag = False
-            elif self.state == "SYNC":
-                if byte == ord("S") and not self.escFlag:
-                    self.dataBuffer = bytearray()
-                elif byte == ord("E") and not self.escFlag:
-                    self.state = "NSYNC"
-                elif byte == ord("/"):
-                    self.escFlag = not self.escFlag
-                else:
-                    if self.escFlag:
-                        self.escFlag = False
-                    self.dataBuffer.append(byte)
-            else:
-                print(f"ERROR: DLL in illegal state ({self.state})")
-        return self.dataBuffer
